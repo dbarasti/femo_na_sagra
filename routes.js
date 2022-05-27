96948dd524e8bf9735438c19a5dcaf3be3032c08
Davide Barasti <d.barasti@studenti.unipi.it>
2022-05-27 15:39:16 +0200

    fix bug not inserting ingredients in empty db

--- routes.js
+++ routes.js
@@ -621,6 +621,9 @@ function loadIngredientsToDB(){
                 type: ingredientType.type,
                 name: ingredient.name,
                 price: ingredient.price
+            }, {upsert:true}, (err, doc) => {
+                if (err) return res.send(500, {error: err});
+                return;
             })
         })
     });
